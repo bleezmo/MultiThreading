c8c50958d0acd4f4baa709d3d679c31228618495
Joshua Rogers <jrogers1@mtb.com>
2019-12-11 08:41:26 -0500

    added javascript test project

---
+++ TestWebApplication/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+(function () {
+    var entries = [];
+    getData();
+    function getData() {
+        entries = [];
+        $.get("/home/getdata", function (data) {
+            for (var i = 0; i < data.length; i++) {
+                entries.push(data[i]);
+                printData(entries);
+            }
+        });
+    }
+    function printData(entries) {
+        var s = "";
+        for (var i = 0; i < entries.length; i++) {
+            s = s + "<p>" + entries[i] + "</p>";
+        }
+        $("#entries").html(s);
+    }
+})();
